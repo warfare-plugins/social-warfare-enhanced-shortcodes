9234b29fe614e0d1604e0e0b6363440a852a7539
DeeDee43 <idellediaz@idelles-mbp.attlocal.net>
2017-05-31 15:58:19 -0500

    deleted initiate plugin function

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -20,14 +20,7 @@ define( 'SWPS_PLUGIN_FILE', __FILE__ );
 define( 'SWPS_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'SWPS_PLUGIN_DIR', dirname( __FILE__ ) );
 
-/**
- * Include the plugin's necessary functions files.
- *
- */
-add_action( 'plugins_loaded' , 'swps_initiate_plugin' , 25 );
-function swps_initiate_plugin() {
 
-}
 
 /**
  * swps_post_twitter_shares() - A function to output the number of twitter shares on a given post.
