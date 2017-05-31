917c193236ab562b461052494fd87ccfc0414ed6
DeeDee43 <idellediaz@idelles-mbp.attlocal.net>
2017-05-31 15:32:09 -0500

    added header info

---
+++ social-warfare-shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Social Warfare-Shortcodes
+ * Plugin URI:  http://warfareplugins.com
+ * Description: A plugin that add cool and useful shortcodes for the Social Warfare plugin.
+ * Version:     1.0.0
+ * Author:      Warfare Plugins
+ * Author URI:  http://warfareplugins.com
+ * Text Domain: social-warfare
+ */
+
+?>
