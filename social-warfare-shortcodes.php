034621895a9a19a0dda5c039da7a75b6918503b2
DeeDee43 <idellediaz@idelles-mbp.attlocal.net>
2017-06-01 12:35:34 -0500

    added site wide short code for facebook shares

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -71,3 +71,17 @@ $shares = get_post_meta( get_the_ID() , '_facebook_shares', true );
 	return $shares;
     }
 }
+
+/**
+ * swps_sitewide_facebook_shares() - A function to output the total number of facebook shares sitewide.
+ *
+ * @param  array $atts An array of parameters parsed from the shortcode attributes
+ * @return string The total number of sitewide shares.
+ *
+ */
+add_shortcode( 'sitewide_facebook_shares', 'swps_sitewide_facebook_shares()' );
+function swps_sitewide_facebook_shares( $atts ) {
+	global $wpdb;
+	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_facebook_shares'" );
+	return swp_kilomega( $sum[0]->total );
+}
