bb53f336f912780954c364f0438fd1d181629ade
DeeDee43 <idellewd@gmail.com>
2017-06-01 14:17:54 -0500

    replaced old sitewide name with "reddit"

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -304,15 +304,15 @@ function swps_sitewide_googlePlus_shares( $atts ) {
    }
 
    /**
-    * swps_sitewide_reddit_shares() - A function to output the total number of hacker_news shares sitewide.
+    * swps_sitewide_reddit_shares() - A function to output the total number of reddit shares sitewide.
     *
     * @param  array $atts An array of parameters parsed from the shortcode attributes
     * @return string The total number of sitewide shares.
     *
     */
-   add_shortcode( 'sitewide_hacker_news_shares', 'swps_sitewide_hacker_news_shares()' );
-   function swps_sitewide_hacker_news_shares( $atts ) {
+   add_shortcode( 'sitewide_reddit_shares', 'swps_sitewide_reddit_shares()' );
+   function swps_sitewide_reddit_shares( $atts ) {
     global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_hacker_news_shares'" );
+    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_reddit_shares'" );
     return swp_kilomega( $sum[0]->total );
    }
