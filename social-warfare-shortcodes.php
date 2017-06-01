e0ab53cb55f21b71ce827c1f0f04f043199d8757
DeeDee43 <idellewd@gmail.com>
2017-06-01 13:33:35 -0500

    added stumble upon short code doc block

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -203,3 +203,11 @@ function swps_sitewide_googlePlus_shares( $atts ) {
   	return $shares;
       }
   }
+
+  /**
+   * swps_sitewide_stumbleupon_shares() - A function to output the total number of stumbleupon shares sitewide.
+   *
+   * @param  array $atts An array of parameters parsed from the shortcode attributes
+   * @return string The total number of sitewide shares.
+   *
+   */
