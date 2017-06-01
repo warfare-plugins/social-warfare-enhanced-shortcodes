77a21f4151bc037ccde24ce8d5ca6c1ab6a3fa99
DeeDee43 <idellediaz@idelles-mbp.attlocal.net>
2017-06-01 13:15:33 -0500

    added linkedIn shares short code

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -151,3 +151,12 @@ function swps_sitewide_googlePlus_shares( $atts ) {
 	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_googlePlus_shares'" );
 	return swp_kilomega( $sum[0]->total );
 }
+
+/**
+ * swps_post_linkedIn_shares() - A function to output the number of linkedIn shares on a given post.
+ *
+ * @since  1.0.0
+ * @param  array $atts An array of parameters parsed from the shortcode.
+ * @return string The number of linkedIn shares formatted accordingly
+ *
+ */
