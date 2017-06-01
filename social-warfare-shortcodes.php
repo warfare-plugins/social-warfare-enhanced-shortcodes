cf759e8f5dc7115173be6ad5e9304d1dee2fce71
DeeDee43 <idellewd@gmail.com>
2017-06-01 14:03:37 -0500

    copied sitewide shortcode for buffer

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -236,3 +236,17 @@ function swps_sitewide_googlePlus_shares( $atts ) {
      return $shares;
        }
    }
+
+   /**
+    * swps_sitewide_twitter_shares() - A function to output the total number of twitter shares sitewide.
+    *
+    * @param  array $atts An array of parameters parsed from the shortcode attributes
+    * @return string The total number of sitewide shares.
+    *
+    */
+   add_shortcode( 'sitewide_twitter_shares', 'swps_sitewide_twitter_shares()' );
+   function swps_sitewide_twitter_shares( $atts ) {
+   	global $wpdb;
+   	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_twitter_shares'" );
+   	return swp_kilomega( $sum[0]->total );
+   }
