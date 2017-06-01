f1699904b0cf56483995320746f3fbac662ff249
DeeDee43 <idellediaz@idelles-mbp.attlocal.net>
2017-06-01 13:09:54 -0500

    added googlePlus sitewide share docblock

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -137,3 +137,17 @@ $shares = get_post_meta( get_the_ID() , '_googlePlus_shares', true );
 	return $shares;
     }
 }
+
+/**
+ * swps_sitewide_googlePlus_shares() - A function to output the total number of googlePlus shares sitewide.
+ *
+ * @param  array $atts An array of parameters parsed from the shortcode attributes
+ * @return string The total number of sitewide shares.
+ *
+ */
+add_shortcode( 'sitewide_twitter_shares', 'swps_sitewide_twitter_shares()' );
+function swps_sitewide_twitter_shares( $atts ) {
+	global $wpdb;
+	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_twitter_shares'" );
+	return swp_kilomega( $sum[0]->total );
+}
