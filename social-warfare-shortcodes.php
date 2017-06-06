a9377f94c584f2af884fc6014a92c4f5f66f8aee
DeeDee43 <idellewd@gmail.com>
2017-06-06 12:53:12 -0500

    Added short code for yummly

--- social-warfare-shortcodes.php
+++ social-warfare-shortcodes.php
@@ -349,3 +349,22 @@ function swps_sitewide_googlePlus_shares( $atts ) {
    	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_tumblr_shares'" );
    	return swp_kilomega( $sum[0]->total );
    }
+
+/**
+ * swps_post_yummly_shares() - A function to output the number of pinterest shares on a given post.
+ *
+ * @since  1.0.0
+ * @param  array $atts An array of parameters parsed from the shortcode.
+ * @return string The number of pinterest shares formatted accordingly
+ *
+ */
+ add_shortcode( 'pinterest_shares', 'swps_post_pinterest_shares' );
+ function swps_post_pinterest_shares( $atts ) {
+     $shares = get_post_meta( get_the_ID() , '_pinterest_shares', true );
+     if( false == $shares ){
+         return 0;
+     } else {
+         $shares = swp_kilomega( $shares );
+         return $shares;
+     }
+ }
