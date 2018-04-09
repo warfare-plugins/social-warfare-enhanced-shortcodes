1eef2743e699878743a19a877e66589824cbfe39
ckmahoney <cortland.mahoney@gmail.com>
2018-04-09 13:49:32 -0400

    Refactored into a class and reduced repeated methods to a loop.

--- social-warfare-enhanced-shortcodes.php
+++ social-warfare-enhanced-shortcodes.php
@@ -11,470 +11,64 @@
 
 defined( 'WPINC' ) || die;
 
-/**
- * Define plugin constants for use throughout the plugin (Version and Directories)
- *
- */
-define( 'SWES_VERSION' , '1.0.0' );
-define( 'SWES_PLUGIN_FILE', __FILE__ );
-define( 'SWES_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
-define( 'SWES_PLUGIN_DIR', dirname( __FILE__ ) );
-define( 'SWES_CORE_VERSION_REQUIRED' , '2.3.2' );
-define( 'SWES_ITEM_ID' , 114492 );
-if ( !defined( 'SWP_ACTIVATE_REGISTRATION_TAB' )) :
-    define( 'SWP_ACTIVATE_REGISTRATION_TAB', true );
-endif;
+add_action( 'plugins_loaded', function() {
+    class SWP_Enhanced_Shortcodes extends SWP_Addon {
+        public function __construct() {
+            $this->name = __( 'Social Warfare - Enhanced Shortcodes', 'social-warfare' );
+            $this->key = 'enhanced_shortcodes';
+            $this->product_id = 114492;
+            $this->version = '1.0.0';
+            $this->core_required = '3.0.0';
 
+            if ( $this->is_registerd() ) {
+                if (version_compare(SWP_VERSION, $this->core_required) >= 0 ) {
+                    $this->add_shortcodes();
+                } else {
+                    throw( "This addon requires Social Warfare version " . $this->core_required . " or higher. Please update our core plugin before activating this one." );
+                }
+            }
+        }
 
-/**
- * Add a registration key for the registration functions
- *
- * @param Array An array of registrations for each paid addon
- * @return Array An array modified to add this new registration key
- *
- */
-add_filter('swp_registrations' , 'social_warfare_enhanced_shortcodes_registration_key' , 20);
-function social_warfare_enhanced_shortcodes_registration_key($array) {
+        public function add_shortcodes() {
+            global $Pro_Options_Page;
 
-    // Make sure core is on a version that contains our dependancies
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAD_CORE_VERSION_REQUIRED) >= 0){
+            $networks = [ 'twitter', 'facebook', 'pinterest', 'google_plus', 'linkedin', 'stumbleupon' ];
 
-        // Add this plugin to the registrations array
-        $array['enhanced_shortcodes'] = array(
-            'plugin_name' => 'Social Warfare - Enhanced Shortcodes',
-            'key' => 'enhanced_shortcodes',
-            'product_id' => SWAD_ITEM_ID
-        );
-    }
+            if ( !empty( $Pro_Options_Page) ) {
+                array_push( $networks, 'buffer', 'hacker_news', 'reddit', 'tumblr', 'yummly' );
+            }
 
-    // Return the modified or unmodified array
-    return $array;
-}
+            foreach($networks as $network ) {
 
-/**
- * The Plugin Update Checker
- *
- *
- * @since 2.0.0 | Created | Update checker added when the plugin was split into core and pro.
- * @since 2.3.3 | 13 SEP 2017 | Updated to use EDD's update checker built into core.
- * @access public
- *
- */
-add_action( 'plugins_loaded' , 'swes_update_checker' , 20 );
-function swes_update_checker() {
+                //* We can't pass an argument to the callback, so save it as an object property.
+                $this->network_key = "${$network}_shares";
 
-    // Make sure core is on a version that contains our dependancies
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWES_CORE_VERSION_REQUIRED) >= 0){
+                add_shortcode( "${network}_shares", [ $this, 'fetch_post_shares' ]);
+                add_shortcode( "sitewide_${network}_shares", [ $this, 'fetch_sitewide_shares' ]);
+            }
 
-        // Check if the plugin is registered
-        if( is_swp_addon_registered( 'enhanced_shortcodes' ) ) {
+            unset $this->network_key;
+        }
 
-            // retrieve our license key from the DB
-            $license_key = swp_get_license_key('enhanced_shortcodes');
-            $website_url = swp_get_site_url();
+        protected function fetch_post_shares() {
+            global $post;
+            get_post_meta( $post->ID, $this->network_key, true );
 
-            // setup the updater
-            $swed_updater = new SW_EDD_SL_Plugin_Updater( SWP_STORE_URL , __FILE__ , array(
-            	'version'   => SWES_VERSION,		// current version number
-            	'license'   => $license_key,	// license key
-            	'item_id'   => SWES_ITEM_ID,	// id of this plugin
-            	'author'    => 'Warfare Plugins',	// author of this plugin
-            	'url'       => $website_url,
-                'beta'      => false // set to true if you wish customers to receive update notifications of beta releases
-                )
-            );
+            return $shares ? swp_kilomega( $shares ) : 0;
+        }
+
+        protected function fetch_sitewide_shares() {
+            global $wpdb;
+            $query = "SELECT
+                    SUM(meta_values)
+                    AS total
+                    FROM $wpdb->postmeta
+                    WHERE meta_key = $this->network_key";
+
+            $total = $wpdb->get_results( $total );
+            return swp_kilomega( $sum[0]->total );
         }
     }
-}
 
-add_action( 'plugins_loaded' , 'swes_initiate_plugin' , 30 );
-function swes_initiate_plugin() {
-    if(defined('SWP_VERSION')){
-        add_shortcode( 'twitter_shares', 'swes_post_twitter_shares' );
-        add_shortcode( 'sitewide_twitter_shares', 'swes_sitewide_twitter_shares' );
-        add_shortcode( 'facebook_shares', 'swes_post_facebook_shares' );
-        add_shortcode( 'sitewide_facebook_shares', 'swes_sitewide_facebook_shares' );
-        add_shortcode( 'pinterest_shares', 'swes_post_pinterest_shares' );
-        add_shortcode( 'sitewide_pinterest_shares', 'swes_sitewide_pinterest_shares' );
-        add_shortcode( 'googlePlus_shares', 'swes_post_googlePlus_shares' );
-        add_shortcode( 'sitewide_googlePlus_shares', 'swes_sitewide_googlePlus_shares' );
-        add_shortcode( 'linkedIn_shares', 'swes_post_linkedIn_shares' );
-        add_shortcode( 'sitewide_linkedIn_shares', 'swes_sitewide_linkedIn_shares' );
-        add_shortcode( 'stumbleupon_shares', 'swes_post_stumbleupon_shares' );
-        add_shortcode( 'sitewide_stumbleupon_shares', 'swes_sitewide_stumbleupon_shares' );
-    }
-    if(defined('SWPP_VERSION')){
-        add_shortcode( 'buffer_shares', 'swes_post_buffer_shares' );
-        add_shortcode( 'sitewide_buffer_shares', 'swes_sitewide_buffer_shares' );
-        add_shortcode( 'hacker_news_shares', 'swes_post_hacker_news_shares' );
-        add_shortcode( 'sitewide_hacker_news_shares', 'swes_sitewide_hacker_news_shares' );
-        add_shortcode( 'reddit_shares', 'swes_post_reddit_shares' );
-        add_shortcode( 'sitewide_reddit_shares', 'swes_sitewide_reddit_shares' );
-        add_shortcode( 'tumblr_shares', 'swes_post_tumblr_shares' );
-        add_shortcode( 'sitewide_tumblr_shares', 'swes_sitewide_tumblr_shares' );
-        add_shortcode( 'yummly_shares', 'swes_post_yummly_shares' );
-        add_shortcode( 'sitewide_yummly_shares', 'swes_sitewide_yummly_shares' );
-    }
-}
-
-/**
- * swes_post_twitter_shares() - A function to output the number of twitter shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of twitter shares formatted accordingly
- *
- */
-function swes_post_twitter_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_twitter_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-	$shares = swp_kilomega( $shares );
-	return $shares;
-    }
-}
-
-/**
- * swes_sitewide_twitter_shares() - A function to output the total number of twitter shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_twitter_shares( $atts ) {
-	global $wpdb;
-	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_twitter_shares'" );
-	return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_facebook_shares() - A function to output the number of facebook shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of facebook shares formatted accordingly
- *
- */
-
-function swes_post_facebook_shares( $atts ) {
-$shares = get_post_meta( get_the_ID() , '_facebook_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-	$shares = swp_kilomega( $shares );
-	return $shares;
-    }
-}
-
-/**
- * swes_sitewide_facebook_shares() - A function to output the total number of facebook shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_facebook_shares( $atts ) {
-	global $wpdb;
-	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_facebook_shares'" );
-	return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_pinterest_shares() - A function to output the number of pinterest shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of pinterest shares formatted accordingly
- *
- */
-
-function swes_post_pinterest_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_pinterest_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_pinterest_shares() - A function to output the total number of pinterest shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_pinterest_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key '_pinterest_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_googlePlus_shares() - A function to output the number of googlePlus shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of googlePlus shares formatted accordingly
- *
- */
-
-function swes_post_googlePlus_shares( $atts ) {
-$shares = get_post_meta( get_the_ID() , '_googlePlus_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-	$shares = swp_kilomega( $shares );
-	return $shares;
-    }
-}
-
-/**
- * swes_sitewide_googlePlus_shares() - A function to output the total number of googlePlus shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_googlePlus_shares( $atts ) {
-	global $wpdb;
-	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_googlePlus_shares'" );
-	return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_linkedIn_shares() - A function to output the number of linkedIn shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of linkedIn shares formatted accordingly
- *
- */
-
-function swes_post_linkedIn_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_linkedIn_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_linkedIn_shares() - A function to output the total number of twitter shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_linkedIn_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_linkedIn_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_stumbleupon_shares() - A function to output the number of stumbleupon shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of stumbleupon shares formatted accordingly
- *
- */
-
-function swes_post_stumbleupon_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_stumbleupon_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_stumbleupon_shares() - A function to output the total number of stumbleupon shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_stumbleupon_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_stumbleupon_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_buffer_shares() - A function to output the number of buffer shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of buffer shares formatted accordingly
- *
- */
-
-function swes_post_buffer_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_buffer_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_buffer_shares() - A function to output the total number of buffer shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_buffer_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_buffer_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_hacker_news_shares() - A function to output the number of hacker_news shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of buffer shares formatted accordingly
- *
- */
-
-function swes_post_hacker_news_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_hacker_news_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_hacker_news_shares() - A function to output the total number of hacker_news shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_hacker_news_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_hacker_news_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_reddit_shares() - A function to output the number of reddit shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of reddit shares formatted accordingly
- *
- */
-
-function swes_post_reddit_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_reddit_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_reddit_shares() - A function to output the total number of reddit shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_reddit_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_reddit_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
-
-/**
- * swes_post_tumblr_shares() - A function to output the number of tumblr shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of tumblr shares formatted accordingly
- *
- */
-
-function swes_post_tumblr_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_tumblr_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_tumblr_shares() - A function to output the total number of tumblr shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_tumblr_shares( $atts ) {
-   	global $wpdb;
-   	$sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_tumblr_shares'" );
-   	return swp_kilomega( $sum[0]->total );
-   }
-
-/**
- * swes_post_yummly_shares() - A function to output the number of yummly shares on a given post.
- *
- * @since  1.0.0
- * @param  array $atts An array of parameters parsed from the shortcode.
- * @return string The number of yummly shares formatted accordingly
- *
- */
-
-function swes_post_yummly_shares( $atts ) {
-    $shares = get_post_meta( get_the_ID() , '_yummly_shares', true );
-    if( false == $shares ){
-        return 0;
-    } else {
-        $shares = swp_kilomega( $shares );
-        return $shares;
-    }
-}
-
-/**
- * swes_sitewide_yummly_shares() - A function to output the total number of tumblr shares sitewide.
- *
- * @param  array $atts An array of parameters parsed from the shortcode attributes
- * @return string The total number of sitewide shares.
- *
- */
-
-function swes_sitewide_yummly_shares( $atts ) {
-    global $wpdb;
-    $sum = $wpdb->get_results( "SELECT SUM(meta_value) AS total FROM $wpdb->postmeta WHERE meta_key = '_yummly_shares'" );
-    return swp_kilomega( $sum[0]->total );
-}
+    new SWP_Enhanced_Shortcodes();
+});
